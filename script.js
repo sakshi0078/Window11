797eb27b2a7f01a55a3610b8e539ec100f818f8d
sakshi0078 <115473896+sakshi0078@users.noreply.github.com>
2023-02-27 09:05:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var taskbar=document.getElementsByClassName("taskbar")[0];
+var startmenu=document.getElementsByClassName("startmenu")[0];
+taskbar.addEventListener('click',function(){
+    if(startmenu.style.bottom=='50px'){
+        startmenu.style.bottom='-655px';
+    }
+    else{
+        startmenu.style.bottom='50px';
+    }
+})
+
